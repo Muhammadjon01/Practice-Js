e8e4c64e0b20021a74897e15b116d50ccae99df9
Muhammadjon01 <75600542+Muhammadjon01@users.noreply.github.com>
2022-06-15 22:29:02 +0500

    Chacking Student

---
+++ Practice with Methods/task1.js
@@ -0,0 +1,9 @@
+let clothes=confirm("Do you have Uniform?")
+let tie=confirm("Do you have tie?")
+let bag=confirm("Do you have bag?")
+
+if(clothes && tie && bag){
+    alert("Welcome")
+}else{
+    alert("Good Bye")
+}
