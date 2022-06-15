970bf3fa119aca81e53210495ad97e18d08e3001
Muhammadjon01 <75600542+Muhammadjon01@users.noreply.github.com>
2022-06-15 23:33:28 +0500

    Solve some  problem

--- Practice with Methods/task3.js
+++ Practice with Methods/task3.js
@@ -1,10 +1,14 @@
-let country =prompt("Enter the name if country")
-let area = +prompt("Enter the area of "+`${country}`)
+let country = prompt("Enter the name if country");
+let area = +prompt("Enter the area of " + `${country}`);
 
-let wroldArea=148940000
+let wroldArea = 148940000;
 
 let findPer;
 
-findPer=(area*100)/wroldArea;
+if (area == "") {
+  alert(`Enter the area of ${country}`);
+} else {
+  findPer = (area * 100) / wroldArea;
 
-alert(`${country}  is ${findPer}% of the total world's landmass`)
+  alert(`${country}  is ${findPer}% of the total world's landmass`);
+}
