0cdd6d2e52668eba1b78c678169697493c759197
Muhammadjon01 <75600542+Muhammadjon01@users.noreply.github.com>
2022-06-19 00:19:14 +0500

    Add task - increment number - with closure function

---
+++ l4-practice with closure/task3.js
@@ -0,0 +1,24 @@
+var decrementNUm = (function () {
+  counter = 0;
+  function changeNum(val) {
+    counter += val;
+  }
+   return {
+     increment: function (val = 0) {
+       changeNum(val + 1);
+     },
+     value: function () {
+       return counter;
+     },
+   };
+})();
+
+
+decrementNUm.increment(5)
+decrementNUm.increment()
+decrementNUm.increment()
+
+decrementNUm.increment()
+
+
+console.log(decrementNUm.value())
