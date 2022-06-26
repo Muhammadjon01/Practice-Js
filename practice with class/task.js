769bf1c2561262f1e51cd578ea3576efbaba9ae3
Muhammadjon01 <75600542+Muhammadjon01@users.noreply.github.com>
2022-06-26 12:42:55 +0500

    Change something

--- practice with class/task.js
+++ practice with class/task.js
@@ -1,37 +1,49 @@
-function student(_name, _course, _year){
-    let name=_name,
-    course=_course,
-    year=_year
- 
-    function getname(){
-     return _name
- }
- 
-    function setname(newName){
-     _name=newName;
-     return newName;
-    }
+function student(_name, _course, _age) {
+  let name = _name,
+    course = _course,
+    age = _age;
 
-    function setcourse(newcourse){
-        _course=newcourse
-        return newcourse
-    }
+  function getname() {
+    return _name;
+  }
 
-   function getcourse(){
-    return _course
-    }
- 
-    return{
-     getname,
-     setname,
-     getcourse,
-     setcourse
-    }
-    
- }
- 
- let Student=new student("Muhammad", "Second",22)
- 
- let Student1=new student("Muhammad", "Thirds",22)
- Student1.setcourse("Five")
- console.log(Student1.getcourse())
+  function setname(newName) {
+    _name = newName;
+    return newName;
+  }
+
+  function setcourse(newcourse) {
+    _course = newcourse;
+    return newcourse;
+  }
+
+  function getcourse() {
+    return _course;
+  }
+
+  function setage(newage) {
+    _age = newage;
+    return newage;
+  }
+
+  function getage(newage) {
+    return _age;
+  }
+
+  return {
+    getname,
+    setname,
+    getcourse,
+    setcourse,
+    getage,
+    setage,
+  };
+}
+
+let Student = new student("Muhammad", "Second", 22);
+
+let Student1 = new student("Muhammad", "Thirds", 22);
+
+Student1.setcourse("Five");
+Student1.setage(21);
+console.log(Student1.getage());
